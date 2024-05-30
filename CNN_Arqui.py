4f191e704d189eaa7d558973bf2195c6591e6fa7
Diego Córdova <60157586+Diego-CB@users.noreply.github.com>
2024-05-30 09:16:28 -0600

    fix: fixed model trainning for current version of tensorwflow

--- CNN_Arqui.py
+++ CNN_Arqui.py
@@ -2,7 +2,7 @@ import cv2
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Rescalar imágenes
 train_data_gen = ImageDataGenerator(rescale=1./255)
@@ -10,19 +10,19 @@ validation_data_gen = ImageDataGenerator(rescale=1./255)
 
 # Preprocesamiento de imágenes para entreno y validación
 train_generator = train_data_gen.flow_from_directory(
-        'data/train',
-        target_size=(48, 48),
-        batch_size=64,
-        color_mode="grayscale",
-        class_mode='categorical'
+    './data/train',
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode='categorical'
 )
 
 validation_generator = validation_data_gen.flow_from_directory(
-        'data/test',
-        target_size=(48, 48),
-        batch_size=64,
-        color_mode="grayscale",
-        class_mode='categorical'
+    './data/test',
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode='categorical'
 )
 
 # Arqui de modelo
@@ -46,15 +46,23 @@ emotion_model.add(Dense(7, activation='softmax'))
 
 cv2.ocl.setUseOpenCL(False)
 
-emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+emotion_model.compile(
+    loss='categorical_crossentropy',
+    optimizer=Adam(
+        # lr=0.0001,
+        # decay=1e-6
+    ), 
+    metrics=['accuracy']
+)
 
 # Entrenar
-emotion_model_info = emotion_model.fit_generator(
-        train_generator,
-        steps_per_epoch=28709 // 64,
-        epochs=50,
-        validation_data=validation_generator,
-        validation_steps=7178 // 64)
+emotion_model_info = emotion_model.fit(
+    train_generator,
+    steps_per_epoch=28709 // 64,
+    epochs=50,
+    validation_data=validation_generator,
+    validation_steps=7178 // 64
+)
 
 
 # Guardar modelo
